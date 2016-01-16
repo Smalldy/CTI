4b6bc01ecf6f85807c166ac6d6036b82c75221b3
小呆 <743682730@qq.com>
2016-01-16 12:03:20 +0800

    修复颜色编码获取器的秒退BUG

---
+++ DBP/colortable.h
@@ -0,0 +1,17 @@
+#ifndef _COLORTABLE_H_
+#define _COLORTABLE_H_
+#include<windows.h>
+struct colornode
+{
+	char * startAddr;
+	char * endAddr;
+	WORD color;
+	struct color * pNext;
+};
+WORD default_color =
+
+
+
+
+
+#endif
